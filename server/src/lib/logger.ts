738039dec1b9087d2f69a388c181d70f764d355b
Anjali <anjalikumari528289@gmail.com>
2026-03-29 13:16:49 +0530

    Delete server directory

--- server/src/lib/logger.ts
+++
@@ -1,5 +0,0 @@
-// Simple console logger for standalone use
-export const logger = {
-  info: (msg: unknown) => console.log("[INFO]", msg),
-  error: (msg: unknown) => console.error("[ERROR]", msg),
-};
